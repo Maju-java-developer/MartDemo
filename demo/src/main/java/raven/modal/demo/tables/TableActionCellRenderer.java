7ce1c53494033cd86d7ce11dc04007a0a8bb985d
Majid Hussain <122609422+Maju-java-developer@users.noreply.github.com>
2025-11-14 15:21:39 +0500

    set action background on row selection

--- demo/src/main/java/raven/modal/demo/tables/TableActionCellRenderer.java
+++ demo/src/main/java/raven/modal/demo/tables/TableActionCellRenderer.java
@@ -8,19 +8,18 @@ import java.awt.*;
 
 public class TableActionCellRenderer extends JPanel implements TableCellRenderer {
 
-    private final ActionItem[] actions;
-
     // Pass the dynamic actions array to the constructor
     public TableActionCellRenderer(ActionItem[] actions) {
-        this.actions = actions;
         setLayout(new FlowLayout(FlowLayout.CENTER, 2, 0));
         setBackground(null);
+        setOpaque(true);
 
         // --- Dynamically Create Buttons ---
         for (ActionItem action : actions) {
             JButton button = new JButton(action.getText(), action.getIcon());
             button.putClientProperty(FlatClientProperties.BUTTON_TYPE, FlatClientProperties.BUTTON_TYPE_ROUND_RECT);
             button.setFocusable(false);
+            button.setOpaque(false); // Button stays flat
             add(button);
         }
     }
@@ -29,8 +28,12 @@ public class TableActionCellRenderer extends JPanel implements TableCellRenderer
     public Component getTableCellRendererComponent(JTable table, Object value,
                                                    boolean isSelected, boolean hasFocus,
                                                    int row, int column) {
-        // You can add logic here to change background/foreground if isSelected is true
-        // but for a JPanel with transparent background, this is usually sufficient.
+
+        if (isSelected) {
+            setBackground(table.getSelectionBackground());
+        } else {
+            setBackground(table.getBackground());
+        }
         return this;
     }
 }
